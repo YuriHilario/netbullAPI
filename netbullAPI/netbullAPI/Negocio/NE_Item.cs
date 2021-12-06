5f04b4273846a40af22f8963585b3ff8f0f16333
ronxss <ronsilva768@gmail.com>
2021-12-06 18:12:09 -0300

    Criação de estrutura referente a entidade item

---
+++ netbullAPI/netbullAPI/Negocio/NE_Item.cs
@@ -0,0 +1,26 @@
+﻿using netbullAPI.Entidade;
+using netbullAPI.Persistencia;
+using netbullAPI.Repository;
+
+namespace netbullAPI.Negocio
+{
+    public class NE_Item
+    {
+        private ItemRepository PE_Item;
+
+        public NE_Item(netbullDBContext PessoaContexto)
+        {
+            PE_Item = new ItemRepository(PessoaContexto);
+        }
+
+        public IEnumerable<Item> BuscaItens()
+        {
+            return PE_Item.BuscaItens();
+        }
+
+        public Item BuscaItemPorId(int id)
+        {
+            return PE_Item.BuscaItemPorId(id);
+        }
+    }
+}
