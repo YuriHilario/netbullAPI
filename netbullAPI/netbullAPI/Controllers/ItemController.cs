8c95583d621bac3ec0474a925b839536af902d48
cassianomedrado <66270366+cassianomedrado@users.noreply.github.com>
2021-12-06 18:51:02 -0300

    Subindo ajustes base controller, e dao

--- netbullAPI/netbullAPI/Controllers/ItemController.cs
+++ netbullAPI/netbullAPI/Controllers/ItemController.cs
@@ -1,40 +1,37 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using netbullAPI.Entidade;
+using netbullAPI.Interfaces;
 using netbullAPI.Negocio;
 using netbullAPI.Persistencia;
-
+using netbullAPI.Util;
 
 namespace netbullAPI.Controllers
 {
-    
+
     [Authorize]
-        [ApiController]
-        [Route("api/[controller]")]
-        public class ItemController : ControllerBase
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ItemController : BaseController
+    {
+        private List<Item> listaItens;
+        public ItemController(INotificador notificador) : base(notificador)
         {
-            private List<Item> listaItens;
-            private NE_Item NE_Item;
-            public ItemController(netbullDBContext ItemContexto)
-            {
-                NE_Item = new NE_Item(ItemContexto);
-            }
+        }
 
-            private NE_Item neItem;
+        //GET: api/<ItemController>
+        [HttpGet]
+        public IEnumerable<Item> Get([FromServices] NE_Item NE_Item)
+        {
+            return NE_Item.BuscaItens();
+        }
 
-            //GET: api/<ItemController>
-            [HttpGet]
-            public IEnumerable<Item> Get()
-            {
-                return NE_Item.BuscaItens();
-            }
-
-            [HttpGet("{id}")]
-            //GET: api/<ItemController>(id)
-            public Item GetPorId(int id)
-            {
-                return NE_Item.BuscaItemPorId(id);
-            }
-        };
-    }
+        [HttpGet("{id}")]
+        //GET: api/<ItemController>(id)
+        public Item GetPorId([FromServices] NE_Item NE_Item, int id)
+        {
+            return NE_Item.BuscaItemPorId(id);
+        }
+    };
+}
 
