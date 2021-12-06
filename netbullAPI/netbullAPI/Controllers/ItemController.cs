3f77ad31f786195dc6cd2603c5491a5e56c5bdd8
ronxss <ronsilva768@gmail.com>
2021-12-06 18:12:09 -0300

    Criação de estrutura referente a entidade item

---
+++ netbullAPI/netbullAPI/Controllers/ItemController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using netbullAPI.Entidade;
+using netbullAPI.Negocio;
+using netbullAPI.Persistencia;
+
+
+namespace netbullAPI.Controllers
+{
+    
+    [Authorize]
+        [ApiController]
+        [Route("api/[controller]")]
+        public class ItemController : ControllerBase
+        {
+            private List<Item> listaItens;
+            private NE_Item NE_Item;
+            public ItemController(netbullDBContext ItemContexto)
+            {
+                NE_Item = new NE_Item(ItemContexto);
+            }
+
+            private NE_Item neItem;
+
+            //GET: api/<ItemController>
+            [HttpGet]
+            public IEnumerable<Item> Get()
+            {
+                return NE_Item.BuscaItens();
+            }
+
+            [HttpGet("{id}")]
+            //GET: api/<ItemController>(id)
+            public Item GetPorId(int id)
+            {
+                return NE_Item.BuscaItemPorId(id);
+            }
+        };
+    }
+
