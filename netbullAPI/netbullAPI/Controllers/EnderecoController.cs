e5fe82eac88882d4997eee13c99c32b33f39a80f
mirandac-alexandre <mirandac.alexandre@gmail.com>
2021-12-08 20:13:38 -0300

    Adicionado authorize e métodos async.

--- netbullAPI/netbullAPI/Controllers/EnderecoController.cs
+++ netbullAPI/netbullAPI/Controllers/EnderecoController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Negocio;
 using netbullAPI.Entidade;
@@ -11,32 +12,28 @@ namespace netbullAPI.Controllers
     [ApiController]
     [Route("api/[controller]")]
     public class EnderecoController : BaseController
-    public class EnderecoController : ControllerBase
     {
         public EnderecoController(INotificador notificador) : base(notificador)
-        public EnderecoController(netbullDBContext EnderecoContexto)
         {
         }
 
         //GET: api/<EnderecoController>
+        [Authorize]
         [HttpGet("{idPessoa}")]
-        public IActionResult /*IEnumerable<Endereco>*/ Get([FromServices] NE_Endereco neEndereco, int idPessoa)
-        public IActionResult /*IEnumerable<Endereco>*/ Get(int idPessoa)
+        public async Task<IActionResult> /*IEnumerable<Endereco>*/ Get([FromServices] NE_Endereco neEndereco, int idPessoa)
         {
-            IEnumerable<Endereco> listaEnderecos = neEndereco.BuscaEnderecosPessoa(idPessoa);
+            IEnumerable<Endereco> listaEnderecos = await neEndereco.BuscaEnderecosPessoa(idPessoa);
 
             if (!listaEnderecos.Any())
                 return NotFound(HttpStatusCode.NotFound);
-                return BadRequest(HttpStatusCode.NotFound);
 
             return Created("Lista obtida", listaEnderecos);
         }
 
-
+        [Authorize]
         [HttpPost]
         // POST api/<EnderecoController>
-        public IActionResult CadastrarNovoEndereco([FromServices] NE_Endereco neEndereco, [FromBody] Endereco endereco)
-        public IActionResult CadastrarNovoEndereco([FromBody] Endereco endereco)
+        public async Task<IActionResult> CadastrarNovoEndereco([FromServices] NE_Endereco neEndereco, [FromBody] Endereco endereco)
         {
             try
             {
@@ -45,15 +42,10 @@ namespace netbullAPI.Controllers
                     Notificar("Endereço vazio.");
                     return NotFound(HttpStatusCode.NoContent);
                 }
-                return BadRequest(HttpStatusCode.NoContent);
 
-                return Ok(neEndereco.CadastraNovoEndereco(endereco) 
+                return Ok(await neEndereco.CadastraNovoEndereco(endereco) 
                     ? new { mensagem = "Inserido com sucesso.", sucesso = true } 
                 : new { mensagem = "Problema ao inserir.", sucesso = false });
-            if (neEndereco.CadastraNovoEndereco(endereco))
-                return Ok();
-            else
-                return BadRequest(HttpStatusCode.BadRequest);
         }
            catch(Exception ex)
             {
@@ -67,9 +59,10 @@ namespace netbullAPI.Controllers
 
         }
 
+        [Authorize]
         [HttpPut]
         // PUT api/<EnderecoController>
-        public IActionResult AtualizaEndereco([FromServices] NE_Endereco neEndereco, Endereco endereco)
+        public async Task<IActionResult> AtualizaEndereco([FromServices] NE_Endereco neEndereco, Endereco endereco)
         {
             try
             {
@@ -79,7 +72,7 @@ namespace netbullAPI.Controllers
                     return NotFound(HttpStatusCode.NoContent);
                 }
 
-                return Ok(neEndereco.AtualizaEndereco(endereco) 
+                return Ok(await neEndereco.AtualizaEndereco(endereco) 
                     ? new { mensagem = "Atualizado com sucesso.", sucesso = true}
                 : new { mensagem = "Problema ao atualizar.", sucesso = false });
             }
@@ -94,22 +87,20 @@ namespace netbullAPI.Controllers
             }
         }
 
+        [Authorize]
         [HttpPatch("{idEndereco}")]
         // PATCH api/<EnderecoController>
-        public IActionResult AtualizaEnderecoPatch([FromServices] NE_Endereco neEndereco, int idEndereco, [FromBody] Endereco endereco)
+        public async Task<IActionResult> AtualizaEnderecoPatch([FromServices] NE_Endereco neEndereco, int idEndereco, [FromBody] Endereco endereco)
         {
             try
             {
                 if (idEndereco == 0)
-        public IActionResult AtualizaEndereco([FromBody] Endereco endereco)
         {
                     Notificar("Id do Endereço ou o logradouro está vazio.");
                     return NotFound(HttpStatusCode.NoContent);
                 }
-            if (endereco.endereco_id == 0 || (endereco.endereco_logradouro == null && endereco.endereco_numero == 0 && endereco.endereco_complemento == null))
-                return BadRequest();
 
-                return Ok(neEndereco.AtualizaEnderecoPatch(idEndereco, endereco) 
+                return Ok(await neEndereco.AtualizaEnderecoPatch(idEndereco, endereco) 
                     ? new { mensagem = "Atualizado com sucesso.", sucesso = true}
                     : new { mensagem = "Problema ao inserir.", sucesso = false });
             }
@@ -122,24 +113,19 @@ namespace netbullAPI.Controllers
                     sucesso = false
                 });
             }
-            if (neEndereco.AtualizaEndereco(endereco.endereco_id, endereco.endereco_logradouro, endereco.endereco_numero, endereco.endereco_complemento))
-                return Ok();
-            else
-                return BadRequest();
         }
 
+        [Authorize]
         [HttpDelete("{idEndereco}")]
-        [HttpDelete]
         // DELETE api/<EnderecoController>
-        public IActionResult ApagaEndereco([FromServices] NE_Endereco neEndereco, int idEndereco)
+        public async Task<IActionResult> ApagaEndereco([FromServices] NE_Endereco neEndereco, int idEndereco)
         {
             try
-        public IActionResult ApagaEndereco(int idEndereco)
         {
             if (idEndereco == 0)
                     return NotFound(new { mensagem = "O idEndereço não foi informado." , sucesso = false});
 
-                var resp = neEndereco.ApagaEndereco(idEndereco);
+                var resp = await neEndereco.ApagaEndereco(idEndereco);
                 if (resp)
                     return Ok(
                         new
@@ -163,10 +149,7 @@ namespace netbullAPI.Controllers
                         mensagem = e.Message,
                         sucesso = false
                     });
-            }
-                return NoContent();
-            
-            return neEndereco.ApagaEndereco(idEndereco) ?  Ok() : BadRequest();
+            }    
         }
     };
 
--- netbullAPI/netbullAPI/Negocio/NE_Endereco.cs
+++ netbullAPI/netbullAPI/Negocio/NE_Endereco.cs
@@ -8,119 +8,34 @@ namespace Negocio
 {
     public class NE_Endereco
     {
-        private netbullDBContext _EnderecoContexto;
-        public NE_Endereco(netbullDBContext EnderecoContexto)
         private DAO_Endereco _daoEndereco;
         public NE_Endereco(DAO_Endereco daoEndereco)
         {
-            _EnderecoContexto = EnderecoContexto;
             _daoEndereco = daoEndereco;
         }
 
-        public IEnumerable<Endereco> BuscaEnderecosPessoa(int idPessoa)
+        public async Task<IEnumerable<Endereco>> BuscaEnderecosPessoa(int idPessoa)
         {
-            return _EnderecoContexto.Enderecos.Where(x => x.endereco_idpessoa == idPessoa);
-            return _daoEndereco.BuscaEnderecosPessoa(idPessoa);
+            return await _daoEndereco.BuscaEnderecosPessoa(idPessoa);
         }
 
-        public bool CadastraNovoEndereco(Endereco endereco)
+        public async Task<bool> CadastraNovoEndereco(Endereco endereco)
         {
-            //verificar se enderçeo já existe
-            try
-            {
-                int encontrouEndereco = (from end in _EnderecoContexto.Enderecos
-                                     where end.endereco_idpessoa == endereco.endereco_idpessoa
-                                     && end.endereco_logradouro == endereco.endereco_logradouro
-                                     && end.endereco_numero == endereco.endereco_numero
-                                     && end.endereco_complemento == endereco.endereco_complemento
-                                     select end).Count();
-                
-                if (encontrouEndereco != 0)
-                    return false;
-                using (_EnderecoContexto)
-                {
-                    _EnderecoContexto.Enderecos.Add(endereco);
-                    _EnderecoContexto.SaveChanges();
-                    return true;
-                }
-            return _daoEndereco.CadastrarNovoEndereco(endereco);
-            }
-            catch(Exception ex)
-            {
-                return false;
-            }
-            
-        }
-
-        public bool AtualizaEndereco(int idEndereco, string? logradouro, int? numero, string? compl)
+            return await _daoEndereco.CadastrarNovoEndereco(endereco);
         }     
-        public bool AtualizaEndereco(Endereco endereco)
+        public async Task<bool> AtualizaEndereco(Endereco endereco)
         {
-            try
-            {
-                Endereco endereco = _EnderecoContexto.Enderecos.Where(x => x.endereco_id == idEndereco).FirstOrDefault();
-                if (endereco == null)
-                    return false;
-                else
-                {
-                    using (_EnderecoContexto)
-                    {
-                        endereco.endereco_logradouro = logradouro == null ? endereco.endereco_logradouro : logradouro;
-                        endereco.endereco_numero = (int)(numero == null ? endereco.endereco_numero : numero);
-                        endereco.endereco_complemento = (compl == null ? endereco.endereco_complemento : compl);
-                        _EnderecoContexto.SaveChanges();
-                        return true;
-                    }
-                }
-            }
-            catch(Exception ex)
-            {
-                return false;
-            }
-
-           return _daoEndereco.AtualizaEndereco(endereco);
+           return await _daoEndereco.AtualizaEndereco(endereco);
         }
 
-        public bool AtualizaLogradouro(int idEndereco, string logradouro)
-        public bool AtualizaEnderecoPatch(int idEndereco, Endereco logradouro)
+        public async Task<bool> AtualizaEnderecoPatch(int idEndereco, Endereco logradouro)
         {
-            try
-            {
-                Endereco endereco = _EnderecoContexto.Enderecos.Where(x => x.endereco_id == idEndereco).FirstOrDefault();
-                if (endereco == null)
-                    return false;
-                else
-                {
-                    using (_EnderecoContexto)
-                    {
-                        endereco.endereco_logradouro = logradouro == null ? endereco.endereco_logradouro : logradouro;
-                        _EnderecoContexto.SaveChanges();
-                        return true;
-                    }
-                }
-            return _daoEndereco.AtualizaEnderecoPatch(idEndereco, logradouro); 
+            return await _daoEndereco.AtualizaEnderecoPatch(idEndereco, logradouro); 
         }
 
-        public bool ApagaEndereco(int idEndereco)
+        public async Task<bool> ApagaEndereco(int idEndereco)
         {
-            try
-            {
-                Endereco endereco = _EnderecoContexto.Enderecos.Where(x => x.endereco_id == idEndereco).FirstOrDefault();
-                if (endereco == null)
-                    return false;
-                else
-                {
-                    _EnderecoContexto.Enderecos.Remove(endereco);
-                    _EnderecoContexto.SaveChanges();
-                    return true;
-                }
-                
-            }
-            catch(Exception ex)
-            {
-                return false;
-            }
-            return _daoEndereco.ApagaEndereco(idEndereco);
+            return await _daoEndereco.ApagaEndereco(idEndereco);
         }
     }
 }
