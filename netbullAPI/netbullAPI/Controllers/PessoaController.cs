5dc938b8f54800e0f1a89dc263daff722af9ba6b
pablo <pabloyuri.s.s.hilario@gmail.com>
2021-12-10 16:29:49 -0300

    Subindo alterações controller pessoa

--- netbullAPI/netbullAPI/Controllers/PessoaController.cs
+++ netbullAPI/netbullAPI/Controllers/PessoaController.cs
@@ -1,37 +1,257 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using netbullAPI.Entidade;
+using netbullAPI.Interfaces;
 using netbullAPI.Negocio;
-using netbullAPI.Persistencia;
+using netbullAPI.Util;
+using System.Net;
 
 namespace netbullAPI.Controllers
 {
     [Authorize]
     [ApiController]
     [Route("api/[controller]")]
-    public class PessoaController : ControllerBase
+    public class PessoaController : BaseController
     {
-        private List<Pessoa> listaPessoas;
-        private NE_Pessoa NE_Aluno;
-        public PessoaController(netbullDBContext PessoaContexto)
-        {
-            NE_Aluno = new NE_Pessoa(PessoaContexto);
-        }
+        public PessoaController(INotificador notificador) : base(notificador) { }
 
-        private NE_Pessoa nePEssoa;
-
-        //GET: api/<AlunosController>
+        /// <summary>
+        /// Busca lista de todas as pessoas casdatradas
+        /// </summary>
+        /// <param name="nePessoa"></param>
+        /// <returns></returns>
         [HttpGet]
-        public IEnumerable<Pessoa> Get()
+        public async Task<IActionResult> Get([FromServices] NE_Pessoa nePessoa)
         {
-            return NE_Aluno.BuscaPessoas();
+            try
+            {
+                var pessoas = await nePessoa.BuscaPessoas();
+                if (pessoas != null)
+                {
+                    return Ok(pessoas);
+                }
+                else
+                {
+                    return NotFound(
+                new
+                {
+                    status = HttpStatusCode.NotFound,
+                    Error = Notificacoes()
+                });
+                }
+                
+            }
+            catch (Exception e)
+            {
+                return NotFound(
+                new
+                {
+                    status = HttpStatusCode.NotFound,
+                    Error = Notificacoes()
+                });
+            }
+
         }
 
+        /// <summary>
+        /// Busca pessas atribuida ao parametro id informado
+        /// </summary>
+        /// <param name="nePessoa"></param>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpGet("{id}")]
-        //GET: api/<AlunosController>(id)
-        public Pessoa GetPorId(int id)
+        public async Task<IActionResult> Get([FromServices] NE_Pessoa nePessoa, int id)
         {
-            return NE_Aluno.BuscaPessoaPorId(id);
+            try
+            {
+                if (id != 0)
+                {
+                    var pessoa = await nePessoa.BuscaPessoaPorId(id);
+                    if (pessoa != null)
+                    {
+                        return Ok(pessoa);
+                    }
+                    else
+                    {
+                        return NotFound(
+                        new
+                        {
+                            status = HttpStatusCode.NotFound,
+                            Error = Notificacoes()
+                        });
+                    }
+
+                }
+                else
+                {
+                    return NotFound(
+                    new
+                    {
+                        status = HttpStatusCode.NotFound,
+                        Error = Notificacoes()
+                    });
+                }
+            }
+            catch
+            {
+                return BadRequest(
+                new
+                {
+                    status = HttpStatusCode.BadRequest,
+                    Error = Notificacoes()
+                });
+            }
+            
         }
-    };     
+
+        /// <summary>
+        /// Cadastra cliente utilizando os parametros informados no objeto pessoa
+        /// </summary>
+        /// <param name="nePessoa"></param>
+        /// <param name="pessoa"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromServices] NE_Pessoa nePessoa, Pessoa pessoa)
+        {
+            try
+            {
+                if (pessoa != null)
+                {
+                    var addPessoa = await nePessoa.InserirPessoa(pessoa);
+                    if (addPessoa)
+                    {
+                        return Ok("Nova Pessoa Inserida com Sucesso!");
+                    }
+                    else
+                    {
+                        return NotFound(
+                    new
+                    {
+                        status = HttpStatusCode.BadRequest,
+                        Error = Notificacoes()
+                    });
+                    }
+
+                }
+                else
+                {
+                    return NotFound(
+                    new
+                    {
+                        status = HttpStatusCode.BadRequest,
+                        Error = Notificacoes()
+                    });
+                }
+            }
+            catch
+            {
+                return BadRequest(
+                new
+                {
+                    status = HttpStatusCode.BadRequest,
+                    Error = Notificacoes()
+                });
+            }
+            
+        }
+
+        /// <summary>
+        /// Realiza alteração na pessoa com o id informa e altera os demais parametros atribuidos
+        /// </summary>
+        /// <param name="nePessoa"></param>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpPut]
+        public async Task<IActionResult> Put([FromServices] NE_Pessoa nePessoa, Pessoa pessoa)
+        {
+            try
+            {
+                if (pessoa != null)
+                {
+                    var attPessoa = await nePessoa.AtualizarPessoa(pessoa);
+                    if (attPessoa != null)
+                    {
+                        return Ok(attPessoa);
+                    }
+                    else
+                    {
+                        return NotFound(
+                    new
+                    {
+                        status = HttpStatusCode.NotFound,
+                        Error = Notificacoes()
+                    });
+                    }
+                }
+                else
+                {
+                    return NotFound(
+                    new
+                    {
+                        status = HttpStatusCode.NotFound,
+                        Error = Notificacoes()
+                    });
+                }
+            }
+            catch
+            {
+                return BadRequest(
+                new
+                {
+                    status = HttpStatusCode.BadRequest,
+                    Error = Notificacoes()
+                });
+            }
+            
+        }
+
+        /// <summary>
+        /// Deleta a pessoa atribuida ao parametro id informado
+        /// </summary>
+        /// <param name="nePessoa"></param>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromServices] NE_Pessoa nePessoa, int id)
+        {
+            try
+            {
+                if (id != 0)
+                {
+                    var delPessoa = await nePessoa.DeletarPessoa(id);
+                    if (delPessoa)
+                    {
+                        return Ok("Pessoa Deletada com Sucesso!");
+                    }
+                    else
+                    {
+                        return NotFound(
+                    new
+                    {
+                        status = HttpStatusCode.NotFound,
+                        Error = Notificacoes()
+                    });
+                    }
+                }
+                else
+                {
+                    return NotFound(
+                    new
+                    {
+                        status = HttpStatusCode.NotFound,
+                        Error = Notificacoes()
+                    });
+                }
+            }
+            catch
+            {
+                return BadRequest(
+                new
+                {
+                    status = HttpStatusCode.BadRequest,
+                    Error = Notificacoes()
+                });
+            }
+        }
+    };
 }
---
+++ netbullAPI/netbullAPI/Persistencia/DAO_Pessoa.cs
@@ -0,0 +1,97 @@
+﻿using netbullAPI.Entidade;
+using netbullAPI.Interfaces;
+using netbullAPI.Util;
+
+namespace netbullAPI.Persistencia
+{
+    public class DAO_Pessoa : DaoBase
+    {
+        private netbullDBContext _PessoaContexto { get; set; }
+        public DAO_Pessoa(INotificador notificador, IConfiguration configuration, netbullDBContext context) : base(notificador, configuration)
+        {
+            _PessoaContexto = context;
+        }
+
+        public async Task<IEnumerable<Pessoa>> BuscaPessoas()
+        {
+            try
+            {
+                var pessoas =  _PessoaContexto.Pessoas.AsEnumerable();
+                return pessoas;
+            }
+            catch (Exception e)
+            {
+                Notificar(e.Message);
+                throw;
+            }
+        }
+
+        public async Task<Pessoa> BuscaPessoaPorId(int id)
+        {
+            try
+            {
+                var pessoa = _PessoaContexto.Pessoas.Where(p => p.pessoa_id == id).FirstOrDefault();
+                return pessoa;
+            }
+            catch (Exception e)
+            {
+                Notificar(e.Message);
+                throw;
+            }
+
+        }
+        
+        public async Task<bool> InserirPessoa(Pessoa pessoa)
+        {
+            try
+            {
+                _PessoaContexto.Pessoas.Add(pessoa);
+                _PessoaContexto.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Notificar(e.Message);
+                throw;
+            }
+
+        }
+        public async Task<Pessoa> UpdatePessoa(Pessoa pessoa)
+        {
+            try
+            {
+                var pessoaSelecionada = _PessoaContexto.Pessoas.Where(p => p.pessoa_id == pessoa.pessoa_id).FirstOrDefault();
+                pessoaSelecionada.pessoa_nome = pessoa.pessoa_nome;
+                pessoaSelecionada.pessoa_tipopessoa = pessoa.pessoa_tipopessoa;
+                pessoaSelecionada.pessoa_documento = pessoa.pessoa_documento;
+                _PessoaContexto.SaveChanges();
+                return pessoaSelecionada;
+            }
+            catch (Exception e)
+            {
+                Notificar(e.Message);
+                throw;
+            }
+
+        }
+
+        public async Task<bool> DeletarPessoa(int id)
+        {
+            try
+            {
+                var pessoaSelecionada = _PessoaContexto.Pessoas.Where(p => p.pessoa_id == id).FirstOrDefault();
+                if (pessoaSelecionada == null)
+                    throw new Exception("Pessoa informada inexistente");
+                _PessoaContexto.Pessoas.Remove(pessoaSelecionada);
+                _PessoaContexto.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Notificar(e.Message);
+                throw;
+            }
+
+        }
+    }
+}
