3bbac8c15cb19bc99dd94bd16223594d41fc0546
Proxyturn <william.a.finardi@gmail.com>
2021-12-09 22:17:19 -0300

    Commit inicial de Pedido

---
+++ netbullAPI/netbullAPI/Controllers/PedidoController.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using netbullAPI.Entidade;
+using netbullAPI.Interfaces;
+using netbullAPI.Negocio;
+using netbullAPI.Util;
+using System.Net;
+
+namespace netbullAPI.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PedidoController : BaseController
+    {
+        public PedidoController(INotificador notificador) : base(notificador) { }
+
+
+        /// <summary>
+        /// Busca lista de telefones atribuidas ao cliente informado
+        /// </summary>
+        /// <param name="ne_pedido"></param>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public IActionResult GetPorId([FromServices] NE_Pedido ne_pedido, int id)
+        {
+            try
+            {
+                var pedidos = ne_pedido.BuscaPedidosCliente(id);
+                if (pedidos == null)
+                    return NotFound(new
+                    {
+                        status = HttpStatusCode.NotFound,
+                        Error = Notificacoes()
+                    });
+                else return Ok(new
+                {
+                    pedidos = ne_pedido.BuscaPedidosCliente(id),
+                    status = HttpStatusCode.OK,
+                    Error = Notificacoes()
+                });
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new
+                {
+                    status = HttpStatusCode.BadRequest,
+                    Error = Notificacoes()
+                });
+            }
+        }
+
+        /// <summary>
+        /// Busca lista de telefones atribuidas ao cliente informado
+        /// </summary>
+        /// <param name="ne_pedido"></param>
+        /// <param name="pedido"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult post([FromServices] NE_Pedido ne_pedido, [FromBody] Pedido pedido)
+        {
+            try
+            {
+                var new_pedido = ne_pedido.AdicionaPedido(pedido);
+                if (new_pedido == null)
+                    return Created($"/{new_pedido.pedido_id}", new_pedido);
+                else
+                    return NotFound(new
+                    {
+                        Status = HttpStatusCode.BadRequest,
+                        Error = Notificacoes()
+                    });
+            }
+            catch(Exception e)
+            {
+                return BadRequest(new
+                {
+                    status = HttpStatusCode.BadRequest,
+                    Error = Notificacoes()
+                });
+            }
+        }
+
+        /// <summary>
+        /// Busca lista de telefones atribuidas ao cliente informado
+        /// </summary>
+        /// <param name="ne_pedido"></param>
+        /// <param name="pedido"></param>
+        /// <returns></returns>
+        [HttpPut]
+        public IActionResult Put([FromServices] NE_Pedido ne_pedido, [FromBody] Pedido pedido)
+        {
+            try
+            {
+                if (ne_pedido.AlteraStatusPedido(pedido))
+                    return Ok(new
+                    {
+                        pedido = pedido,
+                        status = HttpStatusCode.OK,
+                        Error = Notificacoes()
+                    });
+                else
+                    return NotFound(new
+                    {
+                        status = HttpStatusCode.BadRequest,
+                        Error = Notificacoes()
+                    });
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new
+                {
+                    status = HttpStatusCode.BadRequest,
+                    Error = Notificacoes()
+                });
+            }
+        }
+    }
+}
---
+++ netbullAPI/netbullAPI/Entidade/Pedido.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace netbullAPI.Entidade
+{
+    public class Pedido
+    {
+        [Key]
+        [Required]
+        public int pedido_id { get; set; }
+        public DateTime pedido_time { get; set; }
+        [ForeignKey("Endereco")]
+        public int pedido_idEndereco { get; set; }
+        public float pedido_valor { get; set; }
+        [Required]
+        public List<Item> itens { get; set; }
+        [ForeignKey("Pessoa")]
+        public int pedido_idPessoa { get; set; }
+        [ForeignKey("User")]
+        public int pedido_idUsuario { get; set; }
+        public EnumStatusPedido pedido_status { get; set;}
+        //Pedido deve receber uma lista de itens
+        //Pessoa deve receber uma lista de pedidos
+    }
+}
